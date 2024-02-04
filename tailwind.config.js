1d5469931419c2ab9897da3ca02114a1ee15fd75
Victor Bruce <victorbruce82@gmail.com>
2024-02-04 12:05:46 +0000

    define custom background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        softOrange: "var(--soft-orange)",
+        softRed: "var(--soft-red)",
+        offWhite: "var(--off-white)",
+        grayishBlue: "var(--grayish-blue)",
+        darkGrayishBlue: "var(--dark-grayish-blue)",
+        veryDarkBlue: "var(--very-dark-blue)"
+      },
       textColor: {
         softOrange: "var(--soft-orange)",
         softRed: "var(--soft-red)",
